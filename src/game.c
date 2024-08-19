fe3bf0f63b6332fcb74fd11e914ad4c4371b8a98
Andrew Mushel <almushel@gmail.com>
2024-08-19 14:34:42 -0500

    Eat and grow

--- src/game.c
+++ src/game.c
@@ -1,12 +1,16 @@
 #include "raylib.h"
 
 #define cell_size 16
-#define UPDATE_RATE 0.25f
+#define UPDATE_RATE 0.25f/2
+
+typedef struct iVector2 {
+	int x, y;
+} iVector2;
 
 struct snake {
 	int len, cap;
 	int * cells;
-	enum {DIR_NONE, DIR_UP, DIR_RIGHT,DIR_DOWN, DIR_LEFT} dir;
+	iVector2 dir;
 };
 
 int main(void) {
@@ -23,54 +27,68 @@ int main(void) {
 		.len = 1
 	};
 	snake.cells[0] = GetRandomValue(0, map_len-1);
+	int food = snake.cells[0];
+	while (food == snake.cells[0]) {
+		food = GetRandomValue(0, map_len-1);
+	}
 
 	float dt = 0;
 	float update_timer = UPDATE_RATE;
 
 	while(!WindowShouldClose()) {
 		dt = GetFrameTime();
-		update_timer -= dt;
+		update_timer -= dt * (float)(update_timer > 0);
 
-		if	(IsKeyDown(KEY_W)) snake.dir = DIR_UP;
-		else if (IsKeyDown(KEY_A)) snake.dir = DIR_LEFT;
-		else if (IsKeyDown(KEY_S)) snake.dir = DIR_DOWN;
-		else if (IsKeyDown(KEY_D)) snake.dir = DIR_RIGHT;
-
-		if (update_timer <= 0 && snake.dir) {
-			int x = snake.cells[snake.len-1] % map_w;
-			int y = (snake.cells[snake.len-1] - x) / map_w;
-
-			switch(snake.dir) {
-				case DIR_UP:
-					y--;
-					break;
-				case DIR_DOWN:
-					y++;
-					break;
-				case DIR_RIGHT:
-					x++;
-					break;
-				case DIR_LEFT:
-					x--;
-					break;
-				default:
-					break;
-			}
-
-			// Screen wrap
-			x = (x + (int)(x < 0)*map_w) % map_w;
-			y = (y + (int)(y < 0)*map_h) % map_h;
-			int new_index = (y * map_w) + x;
-
-			snake.cells[snake.len-1] = new_index;
+		iVector2 new_dir = {
+			.x = (int)IsKeyDown(KEY_D) - (int)IsKeyDown(KEY_A),
+			.y = (int)IsKeyDown(KEY_S) - (int)IsKeyDown(KEY_W),
+		};
+		// Enforce 4 directions prioritizing horizontal axis
+		new_dir.y *= (int)(new_dir.x == 0);
+		if ( (new_dir.x && !snake.dir.x) || (new_dir.y && !snake.dir.y) ) {
+			snake.dir = new_dir;
 		}
 
 		BeginDrawing();
 		ClearBackground(DARKGRAY);
 
 		for (int i = 0; i < snake.len; i++) {
-			// Update
-			if (i < snake.len-1 && update_timer <= 0) {
+			if (i == snake.len-1) {
+				if ((snake.dir.x||snake.dir.y) && update_timer <= 0) {
+					int x = snake.cells[snake.len-1] % map_w;
+					int y = (snake.cells[snake.len-1] - x) / map_w;
+
+					x += snake.dir.x;
+					y += snake.dir.y;
+					
+					// Screen wrap
+					x = (x + (int)(x < 0)*map_w) % map_w;
+					y = (y + (int)(y < 0)*map_h) % map_h;
+					int new_index = (y * map_w) + x;
+
+					if (new_index == food) {
+						snake.cells[snake.len++] = new_index;
+						i++; // Avoid moving newly grown snake segment
+						
+						food = GetRandomValue(0, map_len);
+						while (true) {
+							bool valid = true;
+							for (int j=0; j<snake.len;j++) {
+								if (snake.cells[j] == food) {
+									valid = false;
+									break;
+								}
+							}
+							
+							if (valid) {
+								break;
+							}
+						}
+					} else {
+						snake.cells[snake.len-1] = new_index;
+					}
+				}
+			} else if (update_timer <= 0) {
 				snake.cells[i] = snake.cells[i+1];
 			}
 
@@ -80,6 +98,10 @@ int main(void) {
 			DrawRectangle(x*cell_size, y*cell_size, cell_size, cell_size, GRAY);
 			DrawRectangleLines(x*cell_size, y*cell_size, cell_size, cell_size, WHITE);
 		}
+
+		int x = food % map_w;
+		int y = (food - x) / map_w;
+		DrawCircle((x*cell_size)+cell_size/2.0f, (y*cell_size)+cell_size/2.0f, cell_size/3.0f, GREEN);
 
 		update_timer += (float)(update_timer <= 0) * UPDATE_RATE;
 		EndDrawing();
