802d539fceab022692b1e423597bff8bf6120def
Andrew Mushel <almushel@gmail.com>
2024-11-04 22:58:10 -0600

    Eliminate snake segment list

--- src/snake.c
+++ src/snake.c
@@ -10,59 +10,96 @@
 #define MAP_COLOR DARKGRAY
 #define OUTLINE_COLOR WHITE
 
+typedef enum Tile_Types {
+	SNAKE_HEAD = 1,
+	SNAKE_UP,
+	SNAKE_RIGHT,
+	SNAKE_DOWN,
+	SNAKE_LEFT,
+	FOOD
+} Tile_Types;
+
 #define zero_array(arr, len) for (int i = 0; i < len; i++) {arr[i] = 0;}
 
 typedef struct iVector2 {
 	int x, y;
 } iVector2;
 
-typedef struct Snake {
-	int len, cap;
-	int * segments;
-	iVector2 dir;
-} Snake;
-
 typedef struct Game_State {
 	enum { PLAY, GAMEOVER, WIN } state;
 	float timer;
 
 	int * map;
-	int map_w, map_h, map_len;
-	int food;
+	int map_w, map_h, map_len, head, tail, score;
 
-	iVector2 next_dir;
-	Snake snake;
+	iVector2 dir, next_dir;
 } Game_State;
 
 void init_game(Game_State* game, int w, int h) {
 	int map_len = w*h;
 	int* map = MemRealloc(game->map, sizeof(int) * map_len);
-	int* segments = MemRealloc(game->snake.segments, sizeof(int) * map_len);
-	if (!map && !segments) {
+	if (!map) {
 		*game = (Game_State){0};
 	}
 
 	zero_array(map, map_len);
 	*game = (Game_State) {
 		.map_len = map_len,
+		.map_w = w, .map_h = h,
 		.map = map,
-		.snake = {
-			.cap = map_len,
-			.segments = segments, 
-			.len = 3
-		},
 		.state = PLAY
 	};
 	
-	game->snake.segments[0] = w * (h/2) + (w/2);
-	game->snake.segments[1] = game->snake.segments[0] - w;
-	game->snake.segments[2] = game->snake.segments[1] - w;
-	for (int s = 0; s < game->snake.len; s++) game->map[game->snake.segments[s]] = 1;
+	game->head = w * (h/2) + (w/2);
+	game->tail = game->head - (2*w);
+	game->map[game->head] = SNAKE_HEAD;
+	game->map[game->head-w] = SNAKE_DOWN;
+	game->map[game->tail] = SNAKE_DOWN;
 	
-	int food = game->snake.segments[0];
+	int food = game->head;
 	while (game->map[ (food = GetRandomValue(0, map_len-1)) ] != 0);
-	game->map[food] = 2;
+	game->map[food] = FOOD;
 }
+
+Tile_Types ivec2_to_snake_dir(iVector2 dir) {
+	if (dir.x == 1 && dir.y == 0){
+		return SNAKE_RIGHT;
+	}
+	if (dir.x == -1 && dir.y == 0){
+		return SNAKE_LEFT;
+	}
+	if (dir.y == 1 && dir.x == 0){
+		return SNAKE_DOWN;
+	}
+	if (dir.y == -1 && dir.x == 0){
+		return SNAKE_UP;
+	}
+
+	return 0;
+}
+
+iVector2 snake_dir_to_ivec2(Tile_Types dir) {
+	iVector2 result = {0};
+	switch(dir) {
+		case SNAKE_DOWN: {
+			result = (iVector2){0,1};
+		} break;
+		case SNAKE_UP: {
+			result = (iVector2){0,-1};
+		} break;
+		case SNAKE_LEFT: {
+			result = (iVector2){-1,0};
+		} break;
+		case SNAKE_RIGHT: {
+			result = (iVector2){1,0};
+		} break;
+
+		default: break;
+	}
+
+	return result;
+}
+
 
 int main(void) {
 	const int map_w = 16;
@@ -78,13 +115,8 @@ int main(void) {
 		TraceLog(LOG_FATAL, "Failed to allocate game map.\n");
 		return 1;
 	}
-	if (!game.snake.segments) {
-		TraceLog(LOG_FATAL, "Failed to allocate snake segments.\n");
-		return 1;
-	}
 
 	while(!WindowShouldClose()) {
-		Snake* snake = &game.snake;
 		switch (game.state) {
 			case PLAY:
 				game.timer -= GetFrameTime() * (float)(game.timer > 0);
@@ -95,52 +127,53 @@ int main(void) {
 				if (IsKeyDown(KEY_D)) game.next_dir = (iVector2){ 1, 0};
 
 				if (game.timer <= 0) {
+					// Check for new input direction and update snake direction
 					if (game.next_dir.x || game.next_dir.y) {
-						int new_dir_index = 
-							snake->segments[snake->len-1]
-							+ game.next_dir.x
-							+ (game.next_dir.y * map_w);
-
-						if (new_dir_index != snake->segments[snake->len-2]) {
-							snake->dir = game.next_dir;
+						// If nonzero value in both current and next dir x or y
+						// input is either 180 degrees (invalid) or the same direction (redundant)
+						// TODO: Fix for first move game over
+						if (!(game.next_dir.x && game.dir.x) && !(game.next_dir.y && game.dir.y)) {
+							game.dir = game.next_dir;
 						}
+
 						game.next_dir = (iVector2){0};
 					}
 
-					if (snake->dir.x || snake->dir.y) {
-						int x = snake->segments[snake->len-1] % map_w;
-						int y = (snake->segments[snake->len-1] - x) / map_w;
-						// Move
-						x += snake->dir.x;
-						y += snake->dir.y;
+					if (game.dir.x || game.dir.y) {
+						int x = game.head % map_w;
+						int y = (game.head - x) / map_w + game.dir.y;
+						x += game.dir.x;
 
-						int new_index = (y * map_w) + x;
+						int new_head = (y * game.map_w) + x;
+
 						bool collision = 
-							game.map[new_index] == 1 ||
-							!CheckCollisionPointRec((Vector2){x,y}, (Rectangle){0,0, map_w, map_h});
+							!CheckCollisionPointRec((Vector2){x,y}, (Rectangle){0,0, game.map_w, game.map_h}) ||
+							(game.map[new_head] > 0 && game.map[new_head] != FOOD);
 
 						if (collision) {
 							game.state = GAMEOVER;
-						} else if (game.map[new_index] == 2) {
-							snake->segments[snake->len++] = new_index;
-								game.map[new_index] = 1;
-								while (game.map[ (game.food = GetRandomValue(0, map_len)) ] != 0){}
+						} else if (game.map[new_head] == FOOD) {
+							game.map[game.head] = ivec2_to_snake_dir(game.dir);
+							game.map[new_head] = SNAKE_HEAD;
+							game.head = new_head;
+							
+							int food = game.head;
+							while (game.map[ (food = GetRandomValue(0, map_len)) ] != 0){}
+							game.map[food] = FOOD;
+							game.score++;
 						} else {
-							zero_array(game.map, game.map_len);
-							game.map[game.food] = 2;
+							game.map[game.head] = ivec2_to_snake_dir(game.dir);
+							game.map[new_head] = SNAKE_HEAD;
+							game.head = new_head;
 
-							for (int i = 0; i < snake->len-1; i++) {
-								snake->segments[i] = snake->segments[i+1];
-								game.map[snake->segments[i]] = 1;
-							}
-
-							snake->segments[snake->len-1] = new_index;
+							iVector2 tail_dir = snake_dir_to_ivec2(game.map[game.tail]);
+							game.map[game.tail] = 0;
+							game.tail += (tail_dir.y * game.map_w) + tail_dir.x;
 						}
-
 					}
 
 					game.timer = UPDATE_INTERVAL;
-					if (snake->len == game.map_len) {
+					if (game.score == game.map_len-2) {
 						game.state = WIN;
 					}
 				}
@@ -155,54 +188,35 @@ int main(void) {
 		BeginDrawing();
 		ClearBackground(MAP_COLOR);
 
-#if INTERP
-		float t = 0.5f-(game.timer/UPDATE_INTERVAL);
-		t *= (float)(snake->dir.x || snake->dir.y);
-		int x = snake->segments[0] % map_w;
-		int y = (snake->segments[0] - x) / map_w;
-		int next_x = x, next_y = y;
-		for (int i = 0; i < snake->len; i++) {
-			if (i < snake->len-1) {
-				next_x = snake->segments[i+1] % map_w;
-				next_y = (snake->segments[i+1] - next_x) / map_w;
-			} else {
-				next_x = x + snake->dir.x;
-				next_y = y + snake->dir.y;
+		int i = 0;
+		for (int y = 0; y < game.map_h; y++) {
+			for (int x = 0; x < game.map_w; x++) {
+				Color fill_color;
+				switch(game.map[i]) {
+					case 0: {
+						i++;
+					} continue;
+
+					case FOOD: {
+						DrawCircle((x*CELL_SIZE)+CELL_SIZE/2.0f, (y*CELL_SIZE)+CELL_SIZE/2.0f, CELL_SIZE/3.0f, GOOD_COLOR);
+					} break;
+
+					default: {
+						if (i == game.head) {
+							fill_color = 
+								(game.state == GAMEOVER) ?
+								BAD_COLOR : GOOD_COLOR;
+						} else {
+							fill_color = SNAKE_COLOR;
+						}
+
+						DrawRectangle(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, fill_color);
+						DrawRectangleLines(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, OUTLINE_COLOR);
+					} break;
+				}
+				i++;
 			}
-			float fx = (float)x + t*(float)(next_x-x);
-			float fy = (float)y + t*(float)(next_y-y);
-			Color fill_color = 
-				(i == snake->len-1) 
-				? (game.state == GAMEOVER)
-					? BAD_COLOR
-					: GOOD_COLOR
-				: SNAKE_COLOR;
-
-			DrawRectangle(fx*CELL_SIZE, fy*CELL_SIZE, CELL_SIZE, CELL_SIZE, fill_color);
-			DrawRectangleLines(fx*CELL_SIZE, fy*CELL_SIZE, CELL_SIZE, CELL_SIZE, OUTLINE_COLOR);
-
-			x = next_x;
-			y = next_y;
 		}
-#else
-		int x,y;
-		for (int i = 0; i < snake->len; i++) {
-			x = snake->segments[i] % map_w;
-			y = (snake->segments[i] - x) / map_w;
-			Color fill_color = 
-				(i == snake->len-1) 
-				? (game.state == GAMEOVER)
-					? BAD_COLOR
-					: GOOD_COLOR
-				: SNAKE_COLOR;
-
-			DrawRectangle(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, fill_color);
-			DrawRectangleLines(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, OUTLINE_COLOR);
-		}
-#endif
-		x = game.food % map_w;
-		y = (game.food - x) / map_w;
-		DrawCircle((x*CELL_SIZE)+CELL_SIZE/2.0f, (y*CELL_SIZE)+CELL_SIZE/2.0f, CELL_SIZE/3.0f, GOOD_COLOR);
 
 		int font_size = CELL_SIZE;
 		int width;
@@ -238,8 +252,7 @@ int main(void) {
 			default: break;
 		}
 
-		int score = snake->len-3;
-		const char* score_text = TextFormat("Score: %d", score);
+		const char* score_text = TextFormat("Score: %d", game.score);
 		width = MeasureText(score_text, font_size);
 
 		DrawText(score_text, (GetScreenWidth()/2) - (width/2), GetScreenHeight() - font_size, font_size, OUTLINE_COLOR);
